ddeb24003a6b4586e06fdc7ae0e2b40af62e76e9
Thiago Bohonos <thiago.bohonos@gmail.com>
2024-04-17 14:55:25 -0300

    Criando a condicional Else

--- 02-condicionais/exemplo1.ts
+++ 02-condicionais/exemplo1.ts
@@ -1,7 +1,9 @@
-const idade: number = 33
+const idade: number = 16
 
 if (idade >= 18) {
     console.log("É maior de idade")
+} else {
+    console.log("É menor de idade")
 }
 
 console.log("Fim do código")
