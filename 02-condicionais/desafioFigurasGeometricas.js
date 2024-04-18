22609d3f6a5c1801575595d7119ae45e1c7a25c5
Thiago Bohonos <thiago.bohonos@gmail.com>
2024-04-18 15:43:13 -0300

    Resolucção do desafio Figuras Geometricas

---
+++ 02-condicionais/desafioFigurasGeometricas.js
@@ -0,0 +1,13 @@
+let quantidadeLados = 4
+
+if (quantidadeLados === 3) {
+    return "TRIANGULO"
+} else if (quantidadeLados === 4) {
+    return "QUADRILATERO" 
+} else if (quantidadeLados === 5) {
+    return "PENTAGONO"
+} else if (quantidadeLados === 6) {
+    return "HEXAGONO"
+} else {
+    return "HEPTAGONO"
+}
