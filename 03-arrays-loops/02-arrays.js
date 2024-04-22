b72a0adc02ea35567f2576e658524cce0bb29469
Thiago Bohonos <thiago.bohonos@gmail.com>
2024-04-22 12:00:28 -0300

    Tamanho de um array

---
+++ 03-arrays-loops/02-arrays.js
@@ -0,0 +1,14 @@
+const frutas = ["banana", "abacaxi", "maçã", "pera", "uva"]
+
+console.log(frutas.length);
+
+frutas[5] = "melão"
+
+console.log(frutas.length);
+
+let i = frutas.length - 1
+console.log(frutas[i]);
+
+frutas[frutas.length] = "abacate"
+
+console.log(frutas);
