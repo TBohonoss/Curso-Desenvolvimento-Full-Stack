000694c35105b75058500a1a0c58a6101852cb21
Thiago Bohonos <thiago.bohonos@gmail.com>
2024-04-22 13:56:55 -0300

    Resolução do exercício 1

---
+++ 03-arrays-loops/exercicio-resolvido1.js
@@ -0,0 +1,16 @@
+const paises = ["Brasil", "Argentina", "Peru", "Uruguai", "Chile"]
+
+paises[paises.length] = "Paraguai"
+
+console.log(paises);
+
+paises[4] = "Bolívia"
+
+console.log(paises);
+
+let i = paises.length - 1;
+console.log(paises[i]);
+
+console.log(paises[1]);
+
+console.log(paises[2]);
