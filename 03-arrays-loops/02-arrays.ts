323a09bbc672e3022a710af5a7b2591589fbeaf2
Thiago Bohonos <thiago.bohonos@gmail.com>
2024-04-22 14:13:23 -0300

    Tipagem de arrays no Typescript

---
+++ 03-arrays-loops/02-arrays.ts
@@ -0,0 +1,13 @@
+const frutas = ["banana", "abacaxi", "maçã", "pera", "uva"]
+
+console.log(frutas.length);
+
+frutas[5] = "melão"
+
+console.log(frutas.length);
+
+console.log(frutas[frutas.length - 1]);
+
+frutas[frutas.length] = "abacate"
+
+console.log(frutas);
---
+++ 03-arrays-loops/03-arrays-typescript.ts
@@ -0,0 +1,5 @@
+const listaFrutas: string[] = ["banana", "abacaxi", "maçã", "pera", "uva"]
+
+listaFrutas[listaFrutas.length] = "abacate";
+
+console.log(listaFrutas);
