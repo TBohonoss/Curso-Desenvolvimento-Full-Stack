c22d0acedf5ae9cae2657f0e5aa9645cade2fb22
Thiago Bohonos <thiago.bohonos@gmail.com>
2024-04-22 11:45:04 -0300

    Como criar arrays

---
+++ 03-arrays-loops/01-arrays.js
@@ -0,0 +1,11 @@
+const thiago = "Thiago"
+const joao = "João"
+const maria = "Maria"
+const pedro = "Pedro"
+
+const listaUsuarios = [thiago, joao, maria, pedro]
+
+console.log(listaUsuarios[0]);
+console.log(listaUsuarios[1]);
+console.log(listaUsuarios[2]);
+console.log(listaUsuarios[3]);
