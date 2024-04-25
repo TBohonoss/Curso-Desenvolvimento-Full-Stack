9602c178f88629054c6b400bfa95ae74601656dc
Thiago Bohonos <thiago.bohonos@gmail.com>
2024-04-24 22:35:37 -0300

    Resolução do exercício 1 de objetos

---
+++ 04-objetos/01-exercicio.js
@@ -0,0 +1,14 @@
+const usuario = {
+    nome: "Thiago Bohonos",
+    idade: 33,
+    altura: 1.75,
+    temCNH: false,
+    habilidades: ["HTML", "CSS", "JavaScript"]
+}
+
+const possuiCNH = usuario.temCNH ? "possui CNH" : "não possui CNH"
+
+console.log(`${usuario.nome} tem ${usuario.idade} anos, ${usuario.altura}m de altura, ${possuiCNH} e as seguintes habilidades:`);
+for (let item of usuario.habilidades) {
+    console.log(`- ${item}`);
+}
