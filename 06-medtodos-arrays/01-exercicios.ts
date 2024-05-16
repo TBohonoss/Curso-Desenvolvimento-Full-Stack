447b50453e0e04b3434c5795034d92bb613f7358
Thiago Bohonos <thiago.bohonos@gmail.com>
2024-05-16 16:34:50 -0300

    Resolução do exercício 1 refernte aos métodos de arrays

---
+++ 06-medtodos-arrays/01-exercicios.ts
@@ -0,0 +1,27 @@
+const paises = ["Brasil", "Argentina", "Uruguai", "Chile"]
+
+paises.push("Peru")
+
+console.log(paises);
+
+paises.pop()
+
+console.log(paises);
+
+paises.unshift("Peru")
+
+console.log(paises);
+
+const ultimoPais = paises.pop()
+
+console.log(ultimoPais);
+
+const segundoPais = paises[1]
+
+console.log(segundoPais);
+
+console.log(paises[2]);
+
+
+
+
